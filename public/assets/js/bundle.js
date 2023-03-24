97de742ba123c6a3ff220abb34831d8096688248
moises.neto <moises.neto@animaeducacao.com.br>
2023-03-23 22:32:25 -0300

    modelo WebPack inicial

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('funciona Ok');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
