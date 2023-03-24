caa4f1b93573f8184da79f5b54503eb1bb77c7be
moises.neto <moises.neto@animaeducacao.com.br>
2023-03-24 14:01:58 -0300

    adaptando css

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,28 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'public', 'assets', 'js' ),
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
+        rules: [
+            {
             exclude: /node_modules/,
             test: /\.js$/,
             use: {
                 loader: 'babel-loader',
                 options: {
                     presets: ['@babel/env']
+                    }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
-        }]
+        ]
     },
     devtool: 'source-map'
 }
